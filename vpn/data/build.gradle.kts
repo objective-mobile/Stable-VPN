bb5aaeccee1bc69a492bea87875fb403bbe36cb5
danya <danya.nosyk@gmail.com>
2025-10-03 12:17:19 +0300

    Environment main

---
+++ vpn/data/build.gradle.kts
@@ -0,0 +1,137 @@
+import com.android.build.gradle.api.ApplicationVariant
+import com.android.build.gradle.api.LibraryVariant
+
+plugins {
+    alias(libs.plugins.kotlin.android)
+    id("com.android.library")
+}
+
+android {
+    buildFeatures {
+        aidl = true
+        buildConfig = true
+    }
+    namespace = "de.blinkt.openvpn"
+    compileSdk = 36
+    //compileSdkPreview = "UpsideDownCake"
+
+    // Also update runcoverity.sh
+    ndkVersion = "28.0.13004108"
+
+    defaultConfig {
+        minSdk = 21
+        externalNativeBuild {
+            cmake {
+                //arguments+= "-DCMAKE_VERBOSE_MAKEFILE=1"
+            }
+        }
+    }
+
+
+    //testOptions.unitTests.isIncludeAndroidResources = true
+
+    externalNativeBuild {
+        cmake {
+            path = File("${projectDir}/src/main/cpp/CMakeLists.txt")
+        }
+    }
+
+    lint {
+        enable += setOf("BackButton", "EasterEgg", "StopShip", "IconExpectedSize", "GradleDynamicVersion", "NewerVersionAvailable")
+        checkOnly += setOf("ImpliedQuantity", "MissingQuantity")
+        disable += setOf("MissingTranslation", "UnsafeNativeCodeLocation")
+    }
+
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
+            isUniversalApk = true
+        }
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+
+}
+
+var swigcmd = "swig"
+// Workaround for macOS(arm64) and macOS(intel) since it otherwise does not find swig and
+// I cannot get the Exec task to respect the PATH environment :(
+if (file("/opt/homebrew/bin/swig").exists())
+    swigcmd = "/opt/homebrew/bin/swig"
+else if (file("/usr/local/bin/swig").exists())
+    swigcmd = "/usr/local/bin/swig"
+
+
+fun registerGenTask(variantName: String, variantDirName: String): File {
+    val baseDir = File(buildDir, "generated/source/ovpn3swig/${variantDirName}")
+    val genDir = File(baseDir, "net/openvpn/ovpn3")
+
+    tasks.register<Exec>("generateOpenVPN3Swig${variantName}")
+    {
+
+        doFirst {
+            mkdir(genDir)
+        }
+        commandLine(listOf(swigcmd, "-outdir", genDir, "-outcurrentdir", "-c++", "-java", "-package", "net.openvpn.ovpn3",
+                "-Isrc/main/cpp/openvpn3/client", "-Isrc/main/cpp/openvpn3/",
+                "-DOPENVPN_PLATFORM_ANDROID",
+                "-o", "${genDir}/ovpncli_wrap.cxx", "-oh", "${genDir}/ovpncli_wrap.h",
+                "src/main/cpp/openvpn3/client/ovpncli.i"))
+        inputs.files( "src/main/cpp/openvpn3/client/ovpncli.i")
+        outputs.dir( genDir)
+
+    }
+    return baseDir
+}
+
+android.libraryVariants.all(object : Action<LibraryVariant> {
+    override fun execute(variant: LibraryVariant) {
+        val sourceDir = registerGenTask(variant.name, variant.baseName.replace("-", "/"))
+        val task = tasks.named("generateOpenVPN3Swig${variant.name}").get()
+
+        variant.registerJavaGeneratingTask(task, sourceDir)
+    }
+})
+
+dependencies {
+    // https://maven.google.com/web/index.html
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.android.view.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.preference.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.security.crypto)
+    implementation(libs.androidx.webkit)
+    implementation(libs.kotlin)
+    implementation(libs.mpandroidchart)
+    implementation(libs.square.okhttp)
+    implementation(project(":vpn:domain"))
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+    implementation("com.google.code.gson:gson:2.11.0")
+}
