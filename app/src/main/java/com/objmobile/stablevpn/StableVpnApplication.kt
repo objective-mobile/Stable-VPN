1fa2c6ee0cfd19042511a912ac8175eff10c1fff
dnosyk <dnosyk@cfg.com.ua>
2025-12-30 10:21:02 +0200

    Firebase initialize

--- app/src/main/java/com/objmobile/stablevpn/StableVpnApplication.kt
+++ app/src/main/java/com/objmobile/stablevpn/StableVpnApplication.kt
@@ -1,10 +1,15 @@
 package com.objmobile.stablevpn
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import com.objmobile.data.StableVpnInitialization
 
 class StableVpnApplication : Application() {
     val vpnInitialization by lazy {
         StableVpnInitialization(this)
+    }
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
     }
 }
