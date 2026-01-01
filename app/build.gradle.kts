23fdfc1f53d8f14b3465635576783c3c54aec8bb
dnosyk <dnosyk@cfg.com.ua>
2026-01-01 16:15:45 +0200

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.objmobile.stablevpn"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "2026.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
